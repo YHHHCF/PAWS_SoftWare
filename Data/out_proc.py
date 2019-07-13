c7511411f473860a37a5bc5647b0c062a235d6a5
YHHHCF <huangzhejinthu@hotmail.com>
2019-07-13 15:18:02 -0400

    output

--- Data/out_proc.py
+++ Data/out_proc.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 input_path = 'toy_output.asc'
 output_path = 'out.png'
 
 pic = np.loadtxt(input_path, skiprows=6)
+pic[pic == 0] = -1
 plt.imshow(pic)
+
+yellow = mpatches.Patch(color='#ffff00', label='hot')
+green = mpatches.Patch(color='#009999', label='cold')
+plt.legend(handles=[yellow, green])
+
 plt.savefig(output_path)
